0a0fda987709abeb43b9bc98413c83a098c7269c
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-09-29 04:01:51 +0000

    Automatically backed up by Learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,6 +6,18 @@ const NavBar = () => {
   return (
     <div>
       {/*{code here}*/}
+      <div className='navbar'>
+     <li>
+       <NavLink to="/" exact>Home</NavLink>
+     </li>
+     <li>
+       <NavLink to="/movies" exact>Movies</NavLink>
+     </li>
+     <li>
+       <NavLink to="/actors" exact>Actors</NavLink></li> 
+     <li>
+       <NavLink to="/directors" exact>Directors</NavLink>
+     </li>
     </div>
   );
 };
